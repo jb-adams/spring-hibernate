3bd777845e711863b3676580d22679da5a4da7e7
Jeremy Adams <jeremy.adams@ga4gh.org>
2020-02-10 21:20:52 -0500

    Feb 10, 2020

--- src/main/java/org/jbadams/spring/Launcher.java
+++ src/main/java/org/jbadams/spring/Launcher.java
@@ -1,32 +1,17 @@
 package org.jbadams.spring;
-import java.util.Arrays;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Launcher {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
-        String runnableId = args[0];
-
-        ClassPathXmlApplicationContext context = 
+        final String runnableId = args[0];
+        final ClassPathXmlApplicationContext context = 
             new ClassPathXmlApplicationContext("applicationContext.xml");
-            Runnable runnable = context.getBean(runnableId, Runnable.class);
+        final Runnable runnable = context.getBean(runnableId, Runnable.class);
             runnable.run(args);
 
             context.close();
-
-
-
-        /*
-        switch(name) {
-            case "CoachApp":
-                CoachApp.run(args);
-                break;
-            case "HelloSpringApp":
-                HelloSpringApp.run(args);
-                break;
-        }
-        */
     }
 
 }
